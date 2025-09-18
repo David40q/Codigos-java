0e1913ed33e2ad467855fa92c711a2e9a010b048
David40q <davidrogelio1@gmail.com>
2025-09-17 20:30:42 -0600

    Agrego proyecto Lista

---
+++ src/list/Llenar.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package list;
+
+/**
+ *
+ * @author Hermanos
+ */
+public class Llenar {
+    public static void main(String[] args) {
+        Lista i= new Lista();
+        i.setVisible(true);
+    }
+}
